4df26942b2c727e0761f32c9b0df99be6dc8588f
SunshineCui <670144242@qq.com>
2018-07-25 17:30:16 +0800

    接入LeakCanary 检测内存泄漏

---
+++ app/src/main/java/com/billy/websocket/base/MyApplication.java
@@ -0,0 +1,33 @@
+package com.billy.websocket.base;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.squareup.leakcanary.RefWatcher;
+
+/**
+ * Created by Billy_Cui on 2018/7/25
+ * Describe:
+ */
+
+public class MyApplication extends Application {
+
+
+    private RefWatcher refWatcher;
+
+    public static RefWatcher getRefWatcher(Context context) {
+        MyApplication application = (MyApplication) context.getApplicationContext();
+        return application.refWatcher;
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        refWatcher = LeakCanary.install(this);
+    }
+}
